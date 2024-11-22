be0d89d1a9aa60dcf893bbb06f0a7fcd06162752
RahmadZikry <rahmadzikry5@gmail.com>
2024-11-23 02:18:49 +0700

    Tahap Awal

---
+++ Projek-GSG/resources/views/Admin/admin_index.blade.php
@@ -0,0 +1,10 @@
+@extends ('layouts',['title'=>'Index Admin'])
+@section('contect')
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-header">Data pasien</h3>
+
+        </div>
+    </div>
+@endsection
+
