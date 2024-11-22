8f4cd0a9bca3c6ee0888eb0c3455ea5c1baa902a
RahmadZikry <rahmadzikry5@gmail.com>
2024-11-23 02:52:58 +0700

    2

--- Projek-GSG/routes/web.php
+++ Projek-GSG/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use app\Http\Controllers\AdminController ;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::resource('admin', AdminController::class) ;
 });
