0527b3477c82c3271cae6c72698602a6f6f059da
RahmadZikry <rahmadzikry5@gmail.com>
2024-12-15 11:04:39 +0700

    12/15/2024

--- Projek-GSG/resources/views/welcome.blade.php
+++ Projek-GSG/resources/views/welcome.blade.php
@@ -68,24 +68,17 @@ https://templatemo.com/tm-569-edu-meeting
 
     <!-- ***** Main Banner Area Start ***** -->
     <section class="section main-banner" id="top" data-section="section1">
-        <video autoplay muted loop id="bg-video">
-            <source src="assets/images/course-video.mp4" type="video/mp4" />
-        </video>
+        <img src="templatemo_569_edu_meeting/assets/images/pcr.jpg" alt="Banner Image" id="bg-image" style="width: 100%; height: auto;"/>
 
         <div class="video-overlay header-text">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="caption">
-                            <h6>Hello Students</h6>
-                            <h2>Welcome to Education</h2>
-                            <p>This is an edu meeting HTML CSS template provided by <a rel="nofollow"
-                                    href="https://templatemo.com/page/1" target="_blank">TemplateMo website</a>. This is
-                                a Bootstrap v5.1.3 layout. The video background is taken from Pexels website, a group of
-                                young people by <a rel="nofollow" href="https://www.pexels.com/@pressmaster"
-                                    target="_blank">Pressmaster</a>.</p>
+                            <h2>GEDUNG SERBA GUNA</h2>
+                            <h6>POLITEKNIK CALTEX RIAU</h6>
                             <div class="main-button-red">
-                                <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+                                <div class="scroll-to-section"><a href="#contact">Watch</a></div>
                             </div>
                         </div>
                     </div>
