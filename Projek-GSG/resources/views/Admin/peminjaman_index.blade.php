03ba591a943e8c31c0bedb28d42f3918cfb81cc6
RahmadZikry <rahmadzikry5@gmail.com>
2025-01-05 02:53:16 +0700

    1/5/2024

--- Projek-GSG/resources/views/Admin/peminjaman_index.blade.php
+++ Projek-GSG/resources/views/Admin/peminjaman_index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layouts_admin',['title'=>'Index Peminjaman'])
+@extends('layouts.layouts_admin', ['title' => 'Index Peminjaman'])
 
 @section('content')
 @if(session('pesan'))
@@ -7,52 +7,53 @@
     </div>
 @endif
 
-    <div class="card">
-        <h3 class="card-header">Peminjaman Fasilitas</h3>
-        <div class="card-body">
-            <h5>Halaman peminjaman Admin</h5>
-            <p>Halaman ini digunakan untuk menampilkan peminjaman fasilitas yang telah diajukan.</p>
+<div class="card">
+    <h3 class="card-header">Peminjaman Fasilitas</h3>
+    <div class="card-body">
+        <h5>Halaman Peminjaman Admin</h5>
+        <p>Halaman ini digunakan untuk menampilkan peminjaman fasilitas yang telah diajukan.</p>
 
-            <table class="table table-striped">
-                <thead>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>User ID</th>
+                    <th>Tanggal Peminjaman</th>
+                    <th>Tanggal Pengembalian</th>
+                    <th>Metode Pembayaran</th>
+                    <th>Status Pembayaran</th>
+                    <th>Status Verifikasi</th>
+                    <th>Bukti Pembayaran</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($peminjaman as $item)
                     <tr>
-                        <th>No</th>
-                        <th>ID Fasilitas</th>
-                        <th>Detail Peminjaman</th>
-                        <th>Tanggal Peminjaman</th>
-                        <th>Jadwal Ketersediaan</th>
-                        <th>Status Verifikasi</th>
-                        <th>Bukti Pembayaran</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($peminjaman as $item)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $item->fasilitas_id }}</td>
-                            <td>{{ $item->user_id}}
-                                <a href="/peminjaman/{{ $item->id }}" class="btn btn-warning btn-sm ml-2">Detail</a>
-                            </td>
-                            <td>{{ $item->tanggal_peminjaman }}</td>
-                            <td>{{ $item->tanggal_pengembalian }}</td>
-                            <td>{{ $item->status_verifikasi }}</td>
-                            <td>
-                                {{ $item->bukti_pembayaran }}
-                                <a href="{{ \Storage::url($item->bukti_pembayaran) }}" target="blank">
-                                    <img src="{{ \Storage::url($item->bukti_pembayaran) }}" alt="" width="50">
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->user_id }}</td>
+                        <td>{{ $item->tanggal_peminjaman }}</td>
+                        <td>{{ $item->tanggal_pengembalian }}</td>
+                        <td>{{ $item->metode_pembayaran }}</td>
+                        <td>{{ $item->status_pembayaran }}</td>
+                        <td>{{ $item->status_verifikasi }}</td>
+                        <td>
+                            @if($item->image)
+                                <a href="{{ \Storage::url($item->image) }}" target="_blank">
+                                    <img src="{{ \Storage::url($item->image) }}" alt="Bukti Pembayaran" width="50">
                                 </a>
-                            </td>
-                            <td>
-                                <a href="/peminjaman/ {{ $item->id  }}" class="btn btn-warning btn-sm">Verifikasi</a>
-
-
-                                
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                            @else
+                                Tidak ada bukti
+                            @endif
+                        </td>
+                        <td>
+                            <a href="/peminjaman/{{ $item->id }}" class="btn btn-primary btn-sm">Detail</a>
+                            <a href="/peminjaman/{{ $item->id }}/verifikasi" class="btn btn-warning btn-sm">Verifikasi</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 @endsection
