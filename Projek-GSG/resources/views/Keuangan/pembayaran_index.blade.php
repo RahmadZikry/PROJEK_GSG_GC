cb3e9f80c5edefcdeeab084b236fd3213a7d5755
RahmadZikry <rahmadzikry5@gmail.com>
2025-01-05 12:23:54 +0700

    1/5/2025

--- Projek-GSG/resources/views/Keuangan/pembayaran_index.blade.php
+++ Projek-GSG/resources/views/Keuangan/pembayaran_index.blade.php
@@ -15,9 +15,10 @@
                         <th>No</th>
                         <th>ID Peminjaman</th>
                         <th>ID User</th>
-                        <th>Tanggal Pembayaran</th>
-                        <th>Status Pembayaran</th>
+                        <th>Tanggal Peminjaman</th>
+                        <th>Tanggal Pengembalian</th>
                         <th>Metode Pembayaran</th>
+                        <th>Status Pembayaran</th>
                         <th>Bukti Pembayaran</th>
                         <th>Aksi</th>
                     </tr>
@@ -28,17 +29,26 @@
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $item->peminjaman_id }}</td>
                             <td>{{ $item->user_id}}</td>
-                            <td>{{ $item->tanggal_pembayaran }}</td>
-                            <td>{{ $item->status_pembayara }}</td>
+                            <td>{{ $item->tanggal_peminjaman }}</td>
+                            <td>{{ $item->tanggal_pengembalian }}</td>
                             <td>{{ $item->metode_pembayaran }}</td>
-                            <td>{{ $item->bukti_pembayaran }}</td>
+                            <td>{{ $item->status_pembayaran }}</td>
                             <td>
-                                <a href="/pembayaran/ {{ $item->id  }}" class="btn btn-warning btn-sm">Konfirmasi</a>
+                                @if($item->image)
+                                    <a href="{{ \Storage::url($item->image) }}" target="_blank">
+                                        <img src="{{ \Storage::url($item->image) }}" alt="Bukti Pembayaran" width="50">
+                                    </a>
+                                @else
+                                    Tidak ada bukti
+                                @endif
+                            </td>
+                            <td>
+                                <a href="/pembayaran/ {{ $item->id  }}" class="btn btn-warning btn-sm">Sukses</a>
                                 <form action="/pembayaran/{{ $item->id }}" method="POST" class="d-inline">
                                     @csrf
                                 @method('delete')
                                     <button class="ml-2 btn btn-danger btn-sm"
-                                    onclick="return confirm('Yakin ingin menghapus data?')">Hapus</button>
+                                    onclick="return confirm('Yakin ingin menghapus data?')">Gagal</button>
                                 </form>
                             </td>
                         </tr>
